87c2ebb9fff81746ed80d29c72dfa49423ba4d35
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2025-10-30 08:25:15 +0100

    Dandy/footer (#7)
    * structure and layout with flex ready
    
    * footer - html grid structure with modals and js
    
    * added styling and content - three forms missing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,23 @@ const navigation = (function () {
     navbar.classList.add('hidden');
   });
 })();
+
+// FUNCTION HANDLER FOR FOOTER LINKS =================================================
+let modalId;
+// open window
+document.querySelectorAll('.footer-link').forEach((btn) =>
+  btn.addEventListener('click', (event) => {
+    // identify button, display matching modal and show wrapper
+    const btnIdent = event.target.id.replace('footer-btn-', '');
+    modalId = 'footer-modal-' + btnIdent;
+    document.getElementById(modalId).classList.remove(`hidden`);
+    document.getElementById('footer-modal-wrapper').classList.remove(`hidden`);
+  })
+);
+// close window
+document.getElementById('footer-modal-wrapper').addEventListener('click', () => {
+  // // close wrapper and hide modal for next use
+  document.getElementById('footer-modal-wrapper').classList.add(`hidden`);
+  // // TODO: close here all modals to avoid problems
+  document.querySelectorAll('.footer-modal').forEach((el) => el.classList.add('hidden'));
+});
